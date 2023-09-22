63356ff54bcb46e59bccfe9381f638acb4d0dd10
gaeguriBanchan <rlawowls93@gamil.com>
2023-09-22 11:00:19 +0900

    데이터베이스 생성, 색상정보 DB에 넣기

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,46 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_calendar_scheduler/database/drift_database.dart';
 import 'package:flutter_calendar_scheduler/screen/home_screen.dart';
 import 'package:intl/date_symbol_data_local.dart';
+
+const DEFAULT_COLORS = [
+  // 빨강
+  'F4436',
+  // 주황
+  'FF9800',
+  // 노랑
+  'FFEB3B',
+  // 초록
+  'FCAF50',
+  // 파랑
+  '2196F3',
+  // 남색
+  '3F51B5',
+  // 보라
+  '9C27B0',
+];
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
   await initializeDateFormatting();
+
+  final database = LocalDatabase();
+
+  final colors = await database.getCategoryColors();
+
+  if (colors.isEmpty) {
+    for (String hexCode in DEFAULT_COLORS) {
+      await database.createCategoryColors(
+        CategoryColorsCompanion(
+          hexCode: Value(hexCode),
+        ),
+      );
+    }
+  }
+
+  print(await database.getCategoryColors());
 
   runApp(
     MaterialApp(
